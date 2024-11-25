81eaae3ced3d955af389ddf986adb7b64ffc8598
laooduu <393917975@qq.com>
2024-11-26 00:36:23 +0800

    resolve deployment error npm ERR! added 826 packages in 41s
    178 packages are looking for funding
      run `npm fund` for details can only install packages when your package.json and package-lock.json

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -1,6 +1,7 @@
 import { RAGChat, upstash } from "@upstash/rag-chat"
 import { redis } from "./redis"
 export const ragChat = new RAGChat({
-    model: upstash("meta-llama/Meta-llama-3-8b-Instruct"),
+    // model: upstash("meta-llama/Meta-llama-3-8b-Instruct"),
+    model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
     redis: redis,
 })
