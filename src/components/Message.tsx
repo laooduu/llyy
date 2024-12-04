9f0e3a813fd614a072796c1003d4d3a4de476c88
laooduu <393917975@qq.com>
2024-12-04 09:39:21 +0800

    update bot and you border color

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,15 +14,16 @@ export const Message = ({ content, isUserMessage }: MessageProps) => {
         })}>
             <div className="p-6">
                 <div className="max-w-3xl mx-auto flex items-start gap-2.5">
-                    <div className={cn("size-10 shrink-0 aspect-square rounded-full border border-zinc-700 bg-zinc-900 flex justify-center items-center", {
-                        "bg-blue-900 border-blue-700 text-zinc-200": isUserMessage,
-                    }
-                    )}>
+                    <div className={
+                        cn(
+                            "size-10 shrink-0 aspect-square rounded-full border border-orange-500 bg-zinc-900 flex justify-center items-center",
+                            { "bg-blue-900 border-blue-700 text-zinc-200": isUserMessage, }
+                        )}>
                         {isUserMessage ? <User className="size-5" /> : <Bot className="size-5 text-white" />}
                     </div>
                     <div className="flex flex-col ml-6 w-full">
                         <div className="flex items-center space-x-2">
-                            <span className="text-sm font-semibold text-white">
+                            <span className="text-sm font-semibold text-teal-500">
                                 {isUserMessage ? "You" : "Bot"}
                             </span>
                         </div>
